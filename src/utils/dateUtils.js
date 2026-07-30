1caff1f23ca73ad411f3a066b20ec34fce07758d
parkhy5454 <parkhy5454@gmail.com>
2026-07-30 16:50:57 +0900

    Add files via upload

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -4,8 +4,8 @@ export const getDaysInMonth = (month, year) => {
 };
 
 export const getMonthName = (month) => {
-  const months = ['January', 'February', 'March', 'April', 'May', 'June',
-                 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = ['1월', '2월', '3월', '4월', '5월', '6월',
+                 '7월', '8월', '9월', '10월', '11월', '12월'];
   return months[month];
 };
 
